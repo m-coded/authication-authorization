028d75e8a1846d0bbd5c45ad8e7db1b97d623ce3
M_Coded <109688722+m-coded@users.noreply.github.com>
2023-09-10 20:46:21 -0700

    Add files via upload

---
+++ authication and authorization/src/main/java/com/authication & authorization/authication ) authorization/authication and authorizationApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce2.ecommerce2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class authication and authorization2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(authication and authorization2Application.class, args);
+	}
+
+}
